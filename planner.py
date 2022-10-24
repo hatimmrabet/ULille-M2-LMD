34742ff5665f0c6b5ae8835c7d3aefe84b187d5e
trkhmd <78960532+trkhmd@users.noreply.github.com>
2022-10-24 17:21:39 +0200

    ajout interval

--- planner.py
+++ planner.py
@@ -1,15 +1,17 @@
+from tabnanny import check
 from crontab import CronTab
+from interval import interval
 
 class planner :
     ## a cron job object
 
     def __init__(self):
-        self.task = None
-        self.hour = None
-        self.minute = None
-        self.day = None
-        self.month = None
-        self.day_of_week = None
+        self.task = ""
+        self.hour = []
+        self.minute = []
+        self.day = []
+        self.month = []
+        self.day_of_week = []
         self.cron = CronTab()
 
     def Task(self, task):
@@ -17,41 +19,92 @@ class planner :
         self.task = task
         return self
 
-    def Hour(self, hour):
-        print('hour: %s' % hour)
-        self.hour = hour
+    def Hour(self, hourparam):
+        
+        if isinstance(hourparam, list):
+            for hour in hourparam:
+                self.Hour(hour)
+        elif isinstance(hourparam, interval):
+            if hourparam.checkHour():
+                self.hour.append(hourparam)
+        else:
+            if hourparam<24 and hourparam>=0:
+                self.hour.append(hourparam)
         return self
     
-    def Minute(self, minute):
-        print('minute: %s' % minute)
-        self.minute = minute
+    def Minute(self, minuteparam):
+        if isinstance(minuteparam, list):
+            for hour in minuteparam:
+                self.Hour(hour)
+        elif isinstance(minuteparam, interval):
+            if interval.checkMinute():
+                self.minute.append(minuteparam)
+        else:
+            if minuteparam<60 and minuteparam>=0:
+                self.minute.append(minuteparam)
         return self
     
-    def Day(self, day):
-        print('day: %s' % day)
-        self.day = day
+    def Day(self, dayparam):
+        if isinstance(dayparam, list):
+            for day in dayparam:
+                self.Day(day)
+        elif isinstance(dayparam, interval):
+            if dayparam.checkDay():
+                self.day.append(dayparam)
+        else:
+            if dayparam<31 and dayparam>=0:
+                self.day.append(dayparam)
         return self
     
-    def Month(self, month):
-        print('month: %s' % month)
-        self.month = month
+    def Month(self, monthparam):
+        if isinstance(monthparam, list):
+            for month in monthparam:
+                self.Month(month)
+        elif isinstance(monthparam, interval):
+            if monthparam.checkMonth():
+                self.month.append(monthparam)
+        else:
+            if monthparam<12 and monthparam>=0:
+                self.month.append(monthparam)
         return self
-    
+        
+
     def Day_of_week(self, day_of_week):
-        print('day_of_week: %s' % day_of_week)
-        self.day_of_week = day_of_week
+        if isinstance(day_of_week, list):
+            for day in day_of_week:
+                self.Day_of_week(day)
+        elif isinstance(day_of_week, interval):
+            if day_of_week.checkDay_of_week():
+                self.day_of_week.append(day_of_week)
+        else:
+            if day_of_week<7 and day_of_week>=0:
+                self.day_of_week.append(day_of_week)
         return self
 
     def __str__(self):
-        return '%s %s %s %s %s %s' % (self.minute, self.hour, self.day, self.month, self.day_of_week, self.task)
+        if self.hour:
+            for hour in self.hour:
+                print('hour: %s' % hour)
+        if self.minute:
+            for minute in self.minute:
+                print('minute: %s' % minute)
+        if self.day:
+            for day in self.day:
+                print('day: %s' % day)
+        if self.month:
+            for month in self.month:
+                print('month: %s' % month)
+        if self.day_of_week:
+            for day_of_week in self.day_of_week:
+                print('day_of_week: %s' % day_of_week)
+        return self.task
 
-    
 
 def main():
     p = planner()
     print("test")
     # 15,45 0-6 */2 * * /usr/local/bin/tache-reguliere.sh
-    p.Task('/usr/local/bin/tache-reguliere.sh').Minute('15,45').Hour('0-6').Day('*/2')
+    p.Task('/usr/local/bin/tache-reguliere.sh').Minute([0,6]).Hour(interval(0,6)).Day(interval(1,31))
     
     print("affichage de p")
     print(p)
