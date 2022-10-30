678a97358a130adbd97fe3053ee22ff97d635799
hatimmrabet <mrabet.hatim2018@gmail.com>
2022-10-30 19:08:32 +0100

    Ajout d'affichage de cron

--- planner.py
+++ planner.py
@@ -70,15 +70,30 @@ class planner :
                 self.day_of_week=day_of_week
         return self
 
+    def affichageCron(self):
+        ret = affichageCron(self.minute,"minute")+" "
+        ret += affichageCron(self.hour,"hour")+" "
+        ret += affichageCron(self.day,"day")+" "
+        ret += affichageCron(self.month,"month")+" "
+        ret += affichageCron(self.day_of_week,"day_of_week")+" "
+        ret += self.task
+        return ret
+        
     def __str__(self):
-        hour_string = affichage(self.hour, "hour")
-        minute_string = affichage(self.minute, "minute")
-        day_string = affichage(self.day, "day")
-        month_string = affichage(self.month, "month")
-        day_of_week_string = affichage(self.day_of_week, "day of week")
+        hour_string = affichageText(self.hour, "hour")
+        minute_string = affichageText(self.minute, "minute")
+        day_string = affichageText(self.day, "day")
+        month_string = affichageText(self.month, "month")
+        day_of_week_string = affichageText(self.day_of_week, "day of week")
         return "Task: %s %s %s %s %s %s" % (self.task, hour_string, minute_string, day_string, month_string, day_of_week_string)
 
-def affichage(param, field):
+def check(param, min, max):
+    if(param[0]<param[1]):
+        if param[0]>=min and param[1]<=max:
+            return True
+    return False
+
+def affichageText(param, field):
     """
     Affichage du parametre en une chaine de characteres
     param: the field to display
@@ -100,15 +115,49 @@ def affichage(param, field):
         return ret
     return ""
 
-def check(param, min, max):
-    if(param[0]<param[1]):
-        if param[0]>=min and param[1]<=max:
-            return True
-    return False
+def affichageCron(param, field):
+    """
+    Affichage du parametre en une chaine de characteres de cron
+    param: the field to display
+    """
+    ret=""
+    if param:
+        max = getMaxMin(field)[1]
+        min = getMaxMin(field)[0]
+        if isinstance(param,tuple):
+            if param[0]==min and param[1]==max:
+                ret+="*"
+            else:
+                ret+= str(param[0])+"-"+str(param[1])
+        elif isinstance(param,list):
+            for i in param:
+                ret+=str(i)+","
+            ret=ret[:-1]
+        elif isinstance(param,int):
+            ret+= str(param)
+    return ret
+
+def getMaxMin(field):
+    """
+    Retourne le max et le min pour un champ
+    param: the field to display
+    """
+    if field == "hour":
+        return (0,23)
+    elif field == "minute":
+        return (0,59)
+    elif field == "day":
+        return (1,31)
+    elif field == "month":
+        return (1,12)
+    elif field == "day_of_week":
+        return (0,6)
+    return None
 
 
 if __name__ == "__main__":
     p = planner()
     # 15,45 0-6 */2 * * /usr/local/bin/tache-reguliere.sh
-    p.Task('/usr/local/bin/tache-reguliere.sh').Hour((0,23)).Minute((6,16)).Day_of_week(1).Month("*").Day((1,31))
+    p.Task('/usr/local/bin/tache-reguliere.sh').Hour((0,22)).Minute((6,16)).Day_of_week(1).Month("*").Day((1,31))
     print(p)
+    print(p.affichageCron())
