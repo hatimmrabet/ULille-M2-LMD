392ecaacb80eba9be4041115ab4638b18cc0c523
trkhmd <78960532+trkhmd@users.noreply.github.com>
2022-11-14 14:08:58 +0100

    ajout pom xml

--- TP2-config/app/src/main/java/lmd/config/App.java
+++ TP2-config/app/src/main/java/lmd/config/App.java
@@ -1,13 +1,17 @@
 package lmd.config;
+import org.petitparser.context.Result;
+import org.petitparser.parser.Parser;
 
-/**
- * Hello world!
- *
- */
+import static org.petitparser.parser.primitive.CharacterParser.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Parser id = letter().seq(letter().or(digit()).star());
+        Result id1 = id.parse("yeah");
+        Result id2 = id.parse("f12");
+        System.out.println(id1.get());
+        System.out.println(id2.get());
     }
 }
